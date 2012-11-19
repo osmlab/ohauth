b657e11828546dd91067c29311857c46adc6e1c2
Tom MacWright <tom@macwright.org>
2012-11-19 17:00:49 -0500

    Makes a node now

--- ohauth.js
+++ ohauth.js
@@ -1,26 +1,32 @@
+(function(context) {
+
 var ohauth = {};
 
-function qsString(obj) {
+ohauth.qsString = function(obj) {
     return Object.keys(obj).sort().map(function(key) {
         return encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]);
     }).join('&');
-}
+};
 
-function stringQs(str) {
+ohauth.sha = sha1();
+
+ohauth.stringQs = function(str) {
     return str.split('&').reduce(function(obj, pair){
         var parts = pair.split('=');
         obj[parts[0]] = (null === parts[1]) ? '' : decodeURIComponent(parts[1]);
         return obj;
     }, {});
-}
+};
 
-ohauth.post = function(url, data, callback) {
+ohauth.xhr = function(method, url, auth, data, options, callback) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (4 == xhr.readyState && 0 !== xhr.status) callback(xhr);
     };
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    var headers = (options && options.header) || { 'Content-Type': 'application/x-www-form-urlencoded' };
+    xhr.open(method, url, true);
+    xhr.setRequestHeader('Authorization', 'OAuth ' + ohauth.authHeader(auth));
+    for (var h in headers) xhr.setRequestHeader(h, headers[h]);
     xhr.send(data);
 };
 
@@ -29,6 +35,12 @@ ohauth.nonce = function() {
         o += '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'[Math.floor(Math.random() * 61)];
     }
     return o;
+};
+
+ohauth.authHeader = function(obj) {
+    return Object.keys(obj).sort().map(function(key) {
+        return encodeURIComponent(key) + '="' + encodeURIComponent(obj[key]) + '"';
+    }).join(', ');
 };
 
 ohauth.timestamp = function() { return ~~((+new Date()) / 1000); };
@@ -40,8 +52,20 @@ ohauth.percentEncode = function(s) {
 };
 
 ohauth.baseString = function(method, url, params) {
+    if (params.oauth_signature) delete params.oauth_signature;
     return [
         method,
         ohauth.percentEncode(url),
-        ohauth.percentEncode(qsString(params))].join('&');
+        ohauth.percentEncode(ohauth.qsString(params))].join('&');
 };
+
+ohauth.signature = function(oauth_secret, token_secret, baseString) {
+    return ohauth.sha.b64_hmac_sha1(
+        ohauth.percentEncode(oauth_secret) + '&' +
+        ohauth.percentEncode(token_secret),
+        baseString);
+};
+
+context.ohauth = ohauth;
+
+})(this);
